2328bc80d3222c281b39e2195ff71636e2480ce8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:00 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "data_factory_pipelines" {
-  description = "All data_factory_pipeline resources"
-  value       = azurerm_data_factory_pipeline.data_factory_pipelines
-}
 output "data_factory_pipelines_activities_json" {
-  description = "List of activities_json values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.activities_json]
+  description = "Map of activities_json values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.activities_json }
 }
 output "data_factory_pipelines_annotations" {
-  description = "List of annotations values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.annotations]
+  description = "Map of annotations values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.annotations }
 }
 output "data_factory_pipelines_concurrency" {
-  description = "List of concurrency values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.concurrency]
+  description = "Map of concurrency values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.concurrency }
 }
 output "data_factory_pipelines_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.data_factory_id }
 }
 output "data_factory_pipelines_description" {
-  description = "List of description values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.description]
+  description = "Map of description values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.description }
 }
 output "data_factory_pipelines_folder" {
-  description = "List of folder values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.folder]
+  description = "Map of folder values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.folder }
 }
 output "data_factory_pipelines_moniter_metrics_after_duration" {
-  description = "List of moniter_metrics_after_duration values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.moniter_metrics_after_duration]
+  description = "Map of moniter_metrics_after_duration values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.moniter_metrics_after_duration }
 }
 output "data_factory_pipelines_name" {
-  description = "List of name values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.name]
+  description = "Map of name values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.name }
 }
 output "data_factory_pipelines_parameters" {
-  description = "List of parameters values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.parameters]
+  description = "Map of parameters values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.parameters }
 }
 output "data_factory_pipelines_variables" {
-  description = "List of variables values across all data_factory_pipelines"
-  value       = [for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : v.variables]
+  description = "Map of variables values across all data_factory_pipelines, keyed the same as var.data_factory_pipelines"
+  value       = { for k, v in azurerm_data_factory_pipeline.data_factory_pipelines : k => v.variables }
 }
 
